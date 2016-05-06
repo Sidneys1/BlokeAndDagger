d9bc98ca7b65917dda1af7bb08a15efa5fd5689a
Sidneys1 <sidneys1@live.com>
2016-05-06 16:00:59 -0400

    Add project files.

---
+++ BlokeAndDagger/Weapons/DamageTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BlokeAndDagger.Weapons {
+    [Flags]
+    public enum DamageTypes {
+        Peircing,
+        Crushing,
+        Slicing,
+        Magic,
+        Meta
+    }
+
+    //public enum DamageDelivery {
+    //    Proximity,
+    //    Projectile,
+    //    Magic
+    //}
+}
